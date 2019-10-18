bbd0f69929660124328cd245363668de18064443
PhilippProzorov <43746355+PhilippProzorov@users.noreply.github.com>
2019-10-18 13:18:09 +0300

    Final minor adjustments

--- src/main/java/grep/Execute.java
+++ src/main/java/grep/Execute.java
@@ -4,11 +4,11 @@ import org.kohsuke.args4j.CmdLineParser;
 import java.util.List;
 
 class Execute {
-    static void main(String[] input) {
+    public static void main(String[] input) {
         if ((input.length == 0) || (input.length == 1)) {
             System.out.println("grep [-v] [-i] [-r] word inputname.txt");
-            return;
         }
+        String[] mainInput = {"-i", "by", "src/samples/input1.txt"};
         Grep grep = new Grep();
         CmdLineParser parser = new CmdLineParser(grep);
         try {
